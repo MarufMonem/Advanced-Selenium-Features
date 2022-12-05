ca83dfa882860fcacbd32f4542aebe7e5fcfb19d
Maruf_Monem <marufmonem@gmail.com>
2022-12-05 09:56:53 +0600

    Basic authentication (JS)

--- src/test/java/windowPopUpAuth.java
+++ src/test/java/windowPopUpAuth.java
@@ -1,2 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class windowPopUpAuth {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeDriver driver = new ChromeDriver();
+        driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+    }
 }
